de721150e39c5e41c9082a18e12cb046ef02ae16
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-11-15 11:44:27 +0700

    feat : notes validator

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,6 +1,7 @@
 class NotesHandler {
-	constructor(service) {
+	constructor(service, validator) {
 		this._service = service;
+		this._validator = validator;
 
 		this.postNoteHandler = this.postNoteHandler.bind(this);
 		this.getNotesHandler = this.getNotesHandler.bind(this);
@@ -11,6 +12,7 @@ class NotesHandler {
 
 	postNoteHandler(request, h) {
 		try {
+			this._validator.validateNotePayload(request.payload);
 			const { title = "untitled", body, tags } = request.payload;
 
 			const noteId = this._service.addNote({ title, body, tags });
@@ -64,15 +66,25 @@ class NotesHandler {
 		}
 	}
 
-	putNoteByIdHandler(request) {
-		const { id } = request.params;
+	putNoteByIdHandler(request, h) {
+		try {
+			this._validator.validateNotePayload(request.payload);
+			const { id } = request.params;
 
-		this._service.editNoteById(id, request.payload);
+			this._service.editNoteById(id, request.payload);
 
-		return {
-			status: "success",
-			message: "Catatan berhasil diperbarui",
-		};
+			return {
+				status: "success",
+				message: "Catatan berhasil diperbarui",
+			};
+		} catch (error) {
+			const response = h.response({
+				status: "fail",
+				message: error.message,
+			});
+			response.code(400);
+			return response;
+		}
 	}
 
 	deleteNoteByIdHandler(request, h) {
