bc0489630d8697e16fb0e4144397ef3cbd55c9d2
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-12-04 17:57:27 +0700

    feat : verify collaborator

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -4,10 +4,12 @@ const InvariantError = require('../../exceptions/InvariantError');
 const { mapDBToModel } = require('../../utils');
 const NotFoundError = require('../../exceptions/NotFoundError');
 const AuthorizationError = require('../../exceptions/AuthorizationError');
+const CollaborationsService = require('./CollaborationsService');
 
 class NotesService {
   constructor() {
     this._pool = new Pool();
+    this._collaborationService = CollaborationsService;
   }
 
   async addNote({
@@ -97,6 +99,21 @@ class NotesService {
 
     if (note.owner !== owner) {
       throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    try {
+      await this.verifyNoteOwner(noteId, userId);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
+      try {
+        await this._collaborationService.verifyCollaborator(noteId, userId);
+      } catch {
+        throw error;
+      }
     }
   }
 }
