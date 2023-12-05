b024b869e10299c718ded9efbf065a1cd4d744b0
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-12-05 13:01:03 +0700

    fix : notes service constructor and notes get and getbyid

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -4,12 +4,11 @@ const InvariantError = require('../../exceptions/InvariantError');
 const { mapDBToModel } = require('../../utils');
 const NotFoundError = require('../../exceptions/NotFoundError');
 const AuthorizationError = require('../../exceptions/AuthorizationError');
-const CollaborationsService = require('./CollaborationsService');
 
 class NotesService {
-  constructor() {
+  constructor(collaborationsService) {
     this._pool = new Pool();
-    this._collaborationService = CollaborationsService;
+    this._collaborationService = collaborationsService;
   }
 
   async addNote({
@@ -35,7 +34,7 @@ class NotesService {
 
   async getNotes(owner) {
     const query = {
-      text: 'SELECT * FROM notes WHERE owner = $1',
+      text: 'SELECT notes.* FROM notes LEFT JOIN collaborations ON collaborations.note_id = notes.id WHERE notes.owner = $1 OR collaborations.user_id = $1 GROUP BY notes.id',
       values: [owner],
     };
     const result = await this._pool.query(query);
@@ -44,7 +43,10 @@ class NotesService {
 
   async getNoteById(id) {
     const query = {
-      text: 'SELECT * FROM notes WHERE id = $1',
+      text: `SELECT notes.* 
+      FROM notes 
+      LEFT JOIN users ON users.id = notes.owner 
+      WHERE notes.id=$1`,
       values: [id],
     };
     const result = await this._pool.query(query);
